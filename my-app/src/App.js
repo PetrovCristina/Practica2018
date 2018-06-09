bf6ee56208ec3055e00b2203bc0191904aaff85e
CristinaBradu <cristina_bradu@mail.ru>
2018-06-09 13:53:09 +0300

    Adding Header and Post components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Post from './components/Post';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+        <Header />
+        <section className="App-main">
+          <Post nickname="Kris" avatar="https://scontent.fkiv1-1.fna.fbcdn.net/v/t1.0-9/26219566_1609899769088786_2309915502285336180_n.jpg?_nc_cat=0&oh=408756361e39ff9ebddc5c8418c47748&oe=5BBFE505" caption="Just what I like!" image="https://images.unsplash.com/photo-1480444423787-9ea7b1509c54?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5b5d4b1832194f5e9847aaeb247622f5&auto=format&fit=crop&w=925&q=80"/>
+            {/* more posts */}
+          </section>
+        </div>
     );
   }
 }
