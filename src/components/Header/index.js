d3367f78ba7764ae253cb632ac746118d5da7b98
CristinaBradu <cristina_bradu@mail.ru>
2018-06-27 12:04:51 +0300

    Create Login page, stylize login&logout buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,11 @@
 // src/components/Header/index.js
 import React from "react";
 import "./Header.css";
-import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends React.Component {
-  login() {
-    window.FB.login(function (response) {
-      if (response.authResponse) {
-        console.log('Welcome!  Fetching your information.... ');
-        window.FB.api('/me', function (response) {
-          console.log('Good to see you, ' + response.name + '.');
-        });
-      } else {
-        console.log('User cancelled login or did not fully authorize.');
-      }
-    });
-  }
+
   logout() {
     window.FB.getLoginStatus(function (response) {
       if (response && response.status === 'connected') {
@@ -35,11 +24,15 @@ class Header extends React.Component {
                     {this.props.token}
           </Link>
         </div>
+
         <Link to="/profile" >
           <div className="Nav-profile" />
         </Link>
-        <button onClick={this.login}>Log In</button>
-        <button onClick={this.logout}>Log Out</button>
+
+        <Link to="/login">
+        <button onClick={this.logout} className="navigation button logout">Log Out</button>
+        </Link>
+
       </nav>
     );
   }
---
+++ src/components/welcome/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./welcome.css";
+import { Link } from 'react-router-dom';
+
+export default class Login extends Component {
+  login() {
+    window.FB.login(function (response) {
+      if (response.authResponse) {
+        console.log('Welcome!  Fetching your information.... ');
+        window.FB.api('/me', function (response) {
+          console.log('Good to see you, ' + response.name + '.');
+        });
+      } else {
+        console.log('User cancelled login or did not fully authorize.');
+      }
+    });
+  }
+  render() {
+    return (
+      <Link to="/">
+      <button onClick={this.login} className="loginBtn loginBtn--facebook">Log In</button>
+      </Link>
+    );
+  }
+}
