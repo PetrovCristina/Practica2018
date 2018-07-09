bc257ae092af17d2e3f64f53523a2a9a76125b96
CristinaBradu <cristina_bradu@mail.ru>
2018-07-09 12:25:21 +0300

    Adding hover on PhotoList. Removing pagination.

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -2,7 +2,9 @@ import React from 'react'
 import unsplash from '../../unsplash'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './style.css'
-import Example from '../Pagination'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Row, Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 class PhotoList extends React.Component {
   state = {
@@ -14,7 +16,7 @@ class PhotoList extends React.Component {
   }
   getPhotos = () =>
     unsplash.photos
-      .listPhotos(1, 15, 'latest')
+      .listPhotos()
       .then(response => response.json())
       .then(photos => {
         console.log(photos)
@@ -26,18 +28,50 @@ class PhotoList extends React.Component {
     const { photos } = this.state
     return photos ? (
       <React.Fragment>
-        <ul>
-          {photos.map(photo => (
-            <li key={photo.id}>
-              <img
-                className="img-thumbnail rounded float-left"
-                src={photo.urls.small}
-                alt={photo.description}
-              />
-            </li>
-          ))}
-        </ul>
-        <Example />
+        <div className="position">
+          <Row>
+            <Col>
+              {photos.map(photo => (
+                <div className="hovereffect" key={photo.id}>
+                  <ul>
+                    <li>
+                      <img
+                        className="img-thumbnail rounded float-left"
+                        src={photo.urls.small}
+                        alt={photo.description}
+                      />
+                    </li>
+                  </ul>
+                  <div className="overlay">
+                    <button className="button button-like">
+                      <FontAwesomeIcon icon="heart" />
+                    </button>
+                    <div className="input-group plus-minus-input">
+                      <div className="input-group-button">
+                        <button
+                          type="button"
+                          className="button square"
+                          data-quantity="plus"
+                          data-field="quantity">
+                          <FontAwesomeIcon icon="plus" />
+                          <span>Collect</span>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="userPic">
+                      <Link to="/">
+                        <img
+                          src={photo.user.profile_image.small}
+                          alt={photo.user.bio}
+                        />
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </Col>
+          </Row>
+        </div>
       </React.Fragment>
     ) : (
       <div>Loading...</div>
