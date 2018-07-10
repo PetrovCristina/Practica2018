9e0a5d291f8c356efb8c209e214986436e6e39c0
CristinaBradu <cristina_bradu@mail.ru>
2018-07-10 12:27:24 +0300

    Creating infinite scrolling. Needs fixing.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import { Container } from 'reactstrap'
 import './App.css'
 import Header from './components/Header'
@@ -23,7 +24,18 @@ library.add(faHeart, faPlus, faSearch, faDownload)
 class App extends Component {
   state = {
     results: [],
-    loading: true
+    loading: true,
+    items: Array.from({ length: 20 })
+  }
+
+  fetchMoreData = () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
+    setTimeout(() => {
+      this.setState({
+        items: this.state.items.concat(Array.from({ length: 20 }))
+      })
+    }, 1500)
   }
 
   render() {
@@ -35,8 +47,17 @@ class App extends Component {
             <Route exact path="/auth/callback" component={AuthCallBack} />
             <Route path="/login" exact component={Login} />
             <Route path="/search" component={Search} />
-            <Route exact path="/" component={PhotoList} />
             <Route path="/profile" component={Profile} />
+            <InfiniteScroll
+              dataLength={this.state.items.length}
+              next={this.fetchMoreData}
+              hasMore={true}
+              loader={<h4>Loading...</h4>}>
+              {this.state.items.map((i, index) => (
+                <Route exact path="/" component={PhotoList} key={index} />
+              ))}
+            </InfiniteScroll>
+
             {/*<Route path="/photo/:id" component={User} />*/}
           </Switch>
           {/* Footer */}
