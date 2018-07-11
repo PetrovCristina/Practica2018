dc80198eb1dbef21091603f19a4f7f9960c60ac5
CristinaBradu <cristina_bradu@mail.ru>
2018-07-11 11:34:58 +0300

    Moving react-infinite from App.js to PhotoList component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { connect } from 'react-redux'
-import InfiniteScroll from 'react-infinite-scroll-component'
 import { Container } from 'reactstrap'
 import './App.css'
 import Header from './components/Header'
@@ -24,18 +23,7 @@ library.add(faHeart, faPlus, faSearch, faDownload)
 class App extends Component {
   state = {
     results: [],
-    loading: true,
-    items: Array.from({ length: 20 })
-  }
-
-  fetchMoreData = () => {
-    // a fake async api call like which sends
-    // 20 more records in 1.5 secs
-    setTimeout(() => {
-      this.setState({
-        items: this.state.items.concat(Array.from({ length: 20 }))
-      })
-    }, 1500)
+    loading: true
   }
 
   render() {
@@ -48,16 +36,7 @@ class App extends Component {
             <Route path="/login" exact component={Login} />
             <Route path="/search" component={Search} />
             <Route path="/profile" component={Profile} />
-            <InfiniteScroll
-              dataLength={this.state.items.length}
-              next={this.fetchMoreData}
-              hasMore={true}
-              loader={<h4>Loading...</h4>}>
-              {this.state.items.map((i, index) => (
-                <Route exact path="/" component={PhotoList} key={index} />
-              ))}
-            </InfiniteScroll>
-
+            <Route exact path="/" component={PhotoList} />
             {/*<Route path="/photo/:id" component={User} />*/}
           </Switch>
           {/* Footer */}
